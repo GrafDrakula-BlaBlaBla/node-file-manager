cf132556c661e5204745b9b19d215d76d8fcec56
bslevchenko <bslevchenko@sberbank.ru>
2024-10-13 16:44:39 +0300

    add message on close script

--- start.js
+++ start.js
@@ -24,26 +24,25 @@ const rl = createInterface({
     // completer: main
 });
 
-// rl.on('line', (line) => {
-//     console.log('mainOn', line)
-// })
+rl.on('line', (line) => {
+    if(line === '.exit') {
+        const message = `Thank you for using File Manager, ${parsedArgsCli.username}, goodbye!`
+        console.log(message)
+    }
+
+    rl.close()
+})
+
+rl.on('SIGINT', () => {
+    const message = `Thank you for using File Manager, ${parsedArgsCli.username}, goodbye!`
+    console.log(message)
+    rl.close()
+})
+
+rl.on('close', () => {
+    exit(0)
+})
 
 // async function main(line) {
 //     console.log('main', line)
-// }
-
-// const argsCli = argv.slice(2)
-
-// const parsedArgsCli = {};
-
-// argsCli.forEach((arg) => {
-//     if (arg.startsWith('--')) {
-//         const [key, value] = arg.split('=')
-//         const name = key.replace(/^--/, '') 
-//         parsedArgsCli[name] = value || true
-//     }
-// })
-
-// if(parsedArgsCli.username) {
-//     console.log(`Welcome to the File Manager, ${parsedArgsCli.username}!`)
 // }
